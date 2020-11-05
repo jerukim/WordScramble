bd273e3d36913a4cf4936e374225eb9d3f6a6bc9
jerukim <mikurej95@gmail.com>
2020-11-04 23:27:16 -0500

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Jeru Kim on 11/4/20.
+//  Copyright © 2020 Jeru Kim. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
