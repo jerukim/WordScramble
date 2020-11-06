0e5059e0ccfc2682d788bc7b1a2f52538de39d40
jerukim <mikurej95@gmail.com>
2020-11-06 00:12:12 -0500

    add new words to list via text field with enter

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -10,11 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     static var fileContents: String {
-            if let fileUrl = Bundle.main.url(forResource: "start", withExtension: "txt") {
-                if let fileContent = try? String(contentsOf: fileUrl) {
-                    return fileContent
-                }
+        if let fileUrl = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let fileContent = try? String(contentsOf: fileUrl) {
+                return fileContent
             }
+        }
         return "file could not be read"
     }
     
@@ -22,9 +22,41 @@ struct ContentView: View {
         return ContentView.fileContents.components(separatedBy: "\n")
     }
     
+    @State private var rootWord = "word"
+    @State private var newWord = ""
+    @State private var usedWords = [String]()
+
+    private func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespaces)
+        
+        guard !answer.isEmpty else {
+            newWord = answer // remove white spaces from answer
+            return
+        }
+        
+        // validate word is actual word & word is made of letters from rootWord before insterting
+        
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+        // ensure that the text field stays selected so user doesn't have to manually reselect
+    }
+    
     var body: some View {
-        List(words, id: \.self) {
-            Text($0.trimmingCharacters(in: .whitespacesAndNewlines))
+        NavigationView {
+            VStack {
+                HStack {
+                    TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .autocapitalization(.none)
+                        .padding()
+                }
+                
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
+            }
+            .navigationBarTitle(rootWord)
         }
     }
 }
